29c775fe51da9877fb28530f9a4ea07573ee13f6
telva0527 <telva0527@gmail.com>
2021-07-05 22:39:21 +0900

    微修正

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module InstagramClone
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    confg.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     config.generators do |g|
       g.assets false
